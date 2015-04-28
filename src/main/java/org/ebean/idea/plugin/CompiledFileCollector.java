c42976259f563c69758eca28345969ce47b8d28c
Yevgeny Krasik <yevgenyk@mythings.com>
2015-04-28 19:59:14 +0300

    License stuff

--- src/main/java/org/ebean/idea/plugin/CompiledFileCollector.java
+++ src/main/java/org/ebean/idea/plugin/CompiledFileCollector.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2015 Yevgeny Krasik
+ *
+ *     This file is part of Ebean-idea-plugin.
+ *
+ *     Ebean-idea-plugin is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Ebean-idea-plugin is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Ebean-idea-plugin.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.ebean.idea.plugin;
 
 import com.intellij.openapi.compiler.CompilationStatusListener;
